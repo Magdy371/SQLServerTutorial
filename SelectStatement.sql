e2286e41df3f68a01f05938685fd330a52cf4608
Magdy Elshrief <73464211+Magdy371@users.noreply.github.com>
2023-05-27 21:17:51 +0300

    SQL Server Tutorial_1
    select and where statement

--- SelectStatement.sql
+++ SelectStatement.sql
@@ -12,3 +12,20 @@ Select EmployeeID From EmployeeSalary where Salary >= 20000
 
 --select top 5 rows from employee salary table
 Select Top 5 * From EmployeeSalary
+
+--Select Distenct values
+Select DISTINCT EmployeeID from EmployeeDemographics
+Select DISTINCT Salary from EmployeeSalary
+
+--select count stataement show you the number of counted var & AS statement
+Select Count(EmployeeID) from EmployeeSalary where Salary > 10000
+Select Count(LastName) As CountedLast_Names from EmployeeDemographics 
+
+-- MAX , MIN , AVG statement
+select MAX(Salary)As Maximum_Salary from EmployeeSalary
+select min(Age) As Lowest_Age from EmployeeDemographics
+--we can say in from statement as follow
+select Avg(Salary) As AverageOf_salary from SQLTutorial.dbo.EmployeeSalary
+
+
+
